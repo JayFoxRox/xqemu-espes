8faae433a0d26eb3b91e0ce1d639503c4f8d9a10
Jannik Vogel <email@jannikvogel.de>
2017-06-25 22:30:23 +0200

    Map memory for DMA access properly (WIP: needs cleanup)

--- hw/xbox/dsp/dsp_dma.c
+++ hw/xbox/dsp/dsp_dma.c
@@ -54,6 +54,36 @@
 # define DPRINTF(s, ...) do { } while (0)
 #endif
 
+static uint32_t dsp_map_memory(DSPDMAState *s, uint32_t addr, int* mem_space)
+{
+    if (addr < 0x1800) {
+        *mem_space = DSP_SPACE_X;
+        return addr;
+    } else if (addr >= 0x1800 && addr < 0x2000) { //?
+        *mem_space = DSP_SPACE_Y;
+        return addr - 0x1800;
+    } else if (addr >= 0x2800 && addr < 0x3800) { //?
+        *mem_space = DSP_SPACE_P;
+        return addr - 0x2800;
+    } else {
+        assert(false);
+    }
+}
+
+static uint32_t dsp_read_mapped_memory(DSPDMAState *s, uint32_t addr)
+{
+    int mem_space;
+    addr = dsp_map_memory(s, addr, &mem_space);
+    return dsp56k_read_memory(s->core, mem_space, addr);
+}
+
+static void dsp_write_mapped_memory(DSPDMAState *s, uint32_t addr, uint32_t value)
+{
+    int mem_space;
+    addr = dsp_map_memory(s, addr, &mem_space);
+    dsp56k_write_memory(s->core, mem_space, addr, value);
+}
+
 static void dsp_dma_run(DSPDMAState *s)
 {
     if (!(s->control & DMA_CONTROL_RUNNING)
@@ -64,13 +94,13 @@ static void dsp_dma_run(DSPDMAState *s)
         uint32_t addr = s->next_block & NODE_POINTER_VAL;
         assert((addr+6) < sizeof(s->core->xram));
 
-        uint32_t next_block = dsp56k_read_memory(s->core, DSP_SPACE_X, addr);
-        uint32_t control = dsp56k_read_memory(s->core, DSP_SPACE_X, addr+1);
-        uint32_t count = dsp56k_read_memory(s->core, DSP_SPACE_X, addr+2);
-        uint32_t dsp_offset = dsp56k_read_memory(s->core, DSP_SPACE_X, addr+3);
-        uint32_t scratch_offset = dsp56k_read_memory(s->core, DSP_SPACE_X, addr+4);
-        uint32_t scratch_base = dsp56k_read_memory(s->core, DSP_SPACE_X, addr+5);
-        uint32_t scratch_size = dsp56k_read_memory(s->core, DSP_SPACE_X, addr+6)+1;
+        uint32_t next_block = dsp_read_mapped_memory(s, addr);
+        uint32_t control = dsp_read_mapped_memory(s, addr+1);
+        uint32_t count = dsp_read_mapped_memory(s, addr+2);
+        uint32_t dsp_offset = dsp_read_mapped_memory(s, addr+3);
+        uint32_t scratch_offset = dsp_read_mapped_memory(s, addr+4);
+        uint32_t scratch_base = dsp_read_mapped_memory(s, addr+5);
+        uint32_t scratch_size = dsp_read_mapped_memory(s, addr+6)+1;
 
         s->next_block = next_block;
         if (s->next_block & NODE_POINTER_EOL) {
@@ -119,31 +149,12 @@ static void dsp_dma_run(DSPDMAState *s)
             scratch_addr = scratch_offset;
         }
 
-        uint32_t mem_address;
-        int mem_space;
-        if (dsp_offset < 0x1800) {
-            assert(dsp_offset+count < 0x1800);
-            mem_space = DSP_SPACE_X;
-            mem_address = dsp_offset;
-        } else if (dsp_offset >= 0x1800 && dsp_offset < 0x2000) { //?
-            assert(dsp_offset+count < 0x2000);
-            mem_space = DSP_SPACE_Y;
-            mem_address = dsp_offset - 0x1800;
-        } else if (dsp_offset >= 0x2800 && dsp_offset < 0x3800) { //?
-            assert(dsp_offset+count < 0x3800);
-            mem_space = DSP_SPACE_P;
-            mem_address = dsp_offset - 0x2800;
-        } else {
-            assert(false);
-        }
-
         uint8_t* scratch_buf = calloc(count, item_size);
 
         if (control & NODE_CONTROL_DIRECTION) {
             int i;
             for (i=0; i<count; i++) {
-                uint32_t v = dsp56k_read_memory(s->core,
-                    mem_space, mem_address+i);
+                uint32_t v = dsp_read_mapped_memory(s, dsp_offset+i);
                 switch(item_size) {
                 case 2:
                     *(uint16_t*)(scratch_buf + i*2) = v;
@@ -180,7 +191,7 @@ static void dsp_dma_run(DSPDMAState *s)
                     break;
                 }
                 // DPRINTF("... %06x\n", v);
-                dsp56k_write_memory(s->core, mem_space, mem_address+i, v);
+                dsp_write_mapped_memory(s, dsp_offset+i, v);
             }
         }
 
